0d8b1b2409d0c705104e2a7cd0d42e3ceff3e4a5
StanleyWangTW <x039516@gmail.com>
2023-11-23 21:09:58 +0800

    edit __init__.py

--- tigersyn/brainage/__init__.py
+++ tigersyn/brainage/__init__.py
@@ -1,1 +1,6 @@
+from predict_brainage import predict_age
 
+if __name__ == '__main__':
+    f = r'tigersyn\brainage\tests\data\CC0001_philips_15_55_M_aseg.nii.gz'
+    model = r'tigersyn\models\aseg_age_v001_linreg.onnx'
+    print(predict_age(f, model))
--- tigersyn/brainage/predict_brainage.py
+++ tigersyn/brainage/predict_brainage.py
@@ -19,9 +19,3 @@ def predict_age(f, model_ff):
     pred_age = sess.run([output_name], {input_name: selected_vols})[0].item()
 
     return pred_age
-
-
-if __name__ == '__main__':
-    f = r'tigersyn\brainage\tests\data\CC0001_philips_15_55_M_aseg.nii.gz'
-    model = r'tigersyn\models\aseg_age_v001_linreg.onnx'
-    print(predict_age(f, model))
