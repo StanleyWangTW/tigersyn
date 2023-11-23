f82889ab5602d06e7424ad30770bbcfc2b10a05a
StanleyWangTW <x039516@gmail.com>
2023-11-23 21:07:17 +0800

    fix get_volumes bug

--- tigersyn/brainage/utils.py
+++ tigersyn/brainage/utils.py
@@ -3,11 +3,12 @@ import nibabel as nib
 
 
 def get_volumes(f, labels):
-    volumes = np.empty(len(labels))
+    volumes = np.zeros(len(labels))
+    img = nib.load(f)
+    data = img.get_fdata()
+
+    voxel_size = np.prod(img.header.get_zooms())
     for idx, lb in enumerate(labels):
-        img = nib.load(f)
-        data = img.get_fdata()
-        voxel_size = np.prod(img.header.get_zooms())
         volumes[idx] = np.sum(data == lb) * voxel_size
 
     return volumes
