f3eecce5e2bf343c923c694af86db51d5c6acf05
StanleyWangTW <x039516@gmail.com>
2023-11-23 21:06:52 +0800

    brainage predict func

---
+++ tigersyn/brainage/predict_brainage.py
@@ -0,0 +1,27 @@
+import onnxruntime as rt
+import numpy as np
+from utils import get_volumes
+
+labels = [
+    2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 26, 28, 41, 42, 43,
+    44, 46, 47, 49, 50, 51, 52, 53, 54, 58, 60
+]
+selected_features_idx = [1, 2, 6, 10, 15, 18, 19, 23, 25, 29]
+
+
+def predict_age(f, model_ff):
+    vols = np.expand_dims(get_volumes(f, labels), axis=0)
+    selected_vols = vols[:, selected_features_idx].astype(np.float32)
+
+    sess = rt.InferenceSession(model_ff, providers=["CPUExecutionProvider"])
+    input_name = sess.get_inputs()[0].name
+    output_name = sess.get_outputs()[0].name
+    pred_age = sess.run([output_name], {input_name: selected_vols})[0].item()
+
+    return pred_age
+
+
+if __name__ == '__main__':
+    f = r'tigersyn\brainage\tests\data\CC0001_philips_15_55_M_aseg.nii.gz'
+    model = r'tigersyn\models\aseg_age_v001_linreg.onnx'
+    print(predict_age(f, model))
