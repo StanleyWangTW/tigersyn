88f1a599d86302fb3b07ed5d67fe659b6325549f
StanleyWangTW <x039516@gmail.com>
2023-11-24 13:22:16 +0800

    edit packagin settings

--- setup.py
+++ setup.py
@@ -6,32 +6,21 @@ with open("README.md", "r") as fh:
 VESION = '0.0.5'
 DESCRIPTION = 'Processing MRI images based on deep-learning'
 CLASSIFIERS = [
-    'Intended Audience :: Developers',
-    'Programming Language :: Python :: 3.9',
+    'Intended Audience :: Developers', 'Programming Language :: Python :: 3.9',
     'License :: OSI Approved :: MIT License',
     "Operating System :: Microsoft :: Windows"
 ]
 
-setup(
-    name='tigersyn',
-    url='https://github.com/StanleyWangTW/tigersyn',
-
-    version=VESION,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    author='X. S. Wang',
-    author_email='',
-    License='MIT',
-
-    packages=find_packages(),
-    classifiers=CLASSIFIERS,
-    python_requires='>=3.9',
-    install_requires=[
-        'numpy',
-        'nibabel',
-        'nilearn',
-        'onnxruntime'
-    ]
-)
+setup(name='tigersyn',
+      url='https://github.com/StanleyWangTW/tigersyn',
+      version=VESION,
+      description=DESCRIPTION,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='X. S. Wang',
+      author_email='',
+      License='MIT',
+      packages=find_packages(),
+      classifiers=CLASSIFIERS,
+      python_requires='>=3.9',
+      install_requires=['numpy', 'nibabel', 'nilearn', 'onnxruntime'])
